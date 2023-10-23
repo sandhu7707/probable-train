2a68ed3d594eba222c92d99e1aa9d0f9efa047a2
unknown <sandhu7707@gmail.com>
2023-10-24 03:04:31 +0530

    added desert and sky

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { TGALoader } from 'three/addons/loaders/TGALoader.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -22,8 +22,9 @@ const ROLE_PLAYER_CONSTANTS = {
     PERSPECTIVE_AXIS: "x", 
     VERTICAL_AXIS: "y", 
     HORIZONTAL_AXIS: "z",
-    SCALE: 0.005,
-    SPEED: 0.2,
+    SCALE: 0.003,
+    SPEED: 0.05,
+    RUNNING_SPEED: 0.1,
     SPEED_MULTIPLIER_ROTATION: 0.5
 };
 
@@ -35,7 +36,7 @@ const CONTROLS = {
 //set these to configure controls
 const keys = CONTROLS.keys;
 
-const spawnPosition = new THREE.Vector3(0, 0, 0);
+const spawnPosition = new THREE.Vector3(113, 5, 323);
 
 const cameraLineOfSightOffset = new THREE.Vector3(0, 0, 0);
 cameraLineOfSightOffset[ROLE_PLAYER_CONSTANTS.VERTICAL_AXIS] = CAMERA_CONSTANTS.LINE_OF_SIGHT_OFFSET;
@@ -57,6 +58,7 @@ const manager = new THREE.LoadingManager();
 
 manager.addHandler(/\.tga$/i, new TGALoader())
 
+var speed = ROLE_PLAYER_CONSTANTS.SPEED;
 var mixer;
 var translatePrespective, rotateOnVertical, rotateOnHorizontal;
 new FBXLoader(manager).load('/phoenix_bird/source/fly.fbx', function(fbx) {
@@ -78,11 +80,17 @@ new FBXLoader(manager).load('/phoenix_bird/source/fly.fbx', function(fbx) {
         console.log(event.code)
         const key = event.code.replace("Key", "")
         keys[key] = true;
+        if(key === 'ShiftLeft'){
+            speed = ROLE_PLAYER_CONSTANTS.RUNNING_SPEED;
+        }
     }
     
     document.onkeyup = function (event) {
         const key = event.code.replace("Key", "")
         keys[key] = false;
+        if(key === 'ShiftLeft'){
+            speed = ROLE_PLAYER_CONSTANTS.SPEED;
+        }
     }
     
 }, undefined, function (error) {
@@ -92,8 +100,50 @@ new FBXLoader(manager).load('/phoenix_bird/source/fly.fbx', function(fbx) {
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-scene.add(new THREE.GridHelper( 40, 40 ));
-scene.add(new THREE.AxesHelper());
+// scene.add(new THREE.GridHelper( 40, 40 ));
+// scene.add(new THREE.AxesHelper());
+
+// const planeWidth = 10000;
+// const planeLength = 10000;
+
+// const geometry = new THREE.PlaneGeometry( planeWidth, planeLength );
+// const texture = new THREE.TextureLoader().load('/sand.jpg')
+// const material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
+
+// const plane = new THREE.Mesh( geometry, material );
+// plane.rotateX(Math.PI/2)
+// scene.add( plane );
+
+var desert;
+new GLTFLoader().load('/desert_map/scene.gltf', function(gltf) {
+    desert = gltf.scene;
+    desert.scale.set(150,150,150)
+    scene.add(desert);
+})
+
+
+// Sky - source : https://github.com/mrdoob/three.js/blob/master/examples/webgl2_volume_cloud.html
+const canvas = document.createElement( 'canvas' );
+canvas.width = 1;
+canvas.height = 32;
+
+const context = canvas.getContext( '2d' );
+const gradient = context.createLinearGradient( 0, 0, 14, 32 );
+gradient.addColorStop( 0.0, '#014a84' );
+gradient.addColorStop( 0.5, '#0561a0' );
+gradient.addColorStop( 1.0, '#437ab6' );
+context.fillStyle = gradient;
+context.fillRect( 0, 0, 1, 32 );
+
+const skyMap = new THREE.CanvasTexture( canvas );
+skyMap.colorSpace = THREE.SRGBColorSpace;
+
+const sky = new THREE.Mesh(
+    new THREE.SphereGeometry( 14000 ),
+    new THREE.MeshBasicMaterial( { map: skyMap, side: THREE.BackSide } )
+);
+scene.add( sky );
+
 
 function animate(){
 
@@ -112,25 +162,25 @@ function animate(){
 
 }
 
-
 function handleMovement() {
     if(keys['Space']){
-        translatePrespective.apply(rolePlayer, [ROLE_PLAYER_CONSTANTS.SPEED]);
+        translatePrespective.apply(rolePlayer, [speed]);
+        console.log(rolePlayer.position)
     }
      if(keys['ControlLeft']){ 
-        translatePrespective.apply(rolePlayer, [-ROLE_PLAYER_CONSTANTS.SPEED]);    
+        translatePrespective.apply(rolePlayer, [-speed]);    
     }
     if(keys['A']){ 
-        rotateOnVertical.apply(rolePlayer, [ROLE_PLAYER_CONSTANTS.SPEED*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);
+        rotateOnVertical.apply(rolePlayer, [speed*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);
     }
     if(keys['D']){
-        rotateOnVertical.apply(rolePlayer, [-ROLE_PLAYER_CONSTANTS.SPEED*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);
+        rotateOnVertical.apply(rolePlayer, [-speed*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);
     }
     if(keys['S']){ 
-        rotateOnHorizontal.apply(rolePlayer, [-ROLE_PLAYER_CONSTANTS.SPEED*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);    
+        rotateOnHorizontal.apply(rolePlayer, [-speed*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);    
     }   
     if(keys['W']){ 
-        rotateOnHorizontal.apply(rolePlayer, [ROLE_PLAYER_CONSTANTS.SPEED*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);    
+        rotateOnHorizontal.apply(rolePlayer, [speed*ROLE_PLAYER_CONSTANTS.SPEED_MULTIPLIER_ROTATION]);    
     }
 }
 
@@ -151,7 +201,7 @@ function updateCamera() {
 }
 
 function setAmbientLight() {
-    scene.add(new THREE.AmbientLight())
+    scene.add(new THREE.AmbientLight( 0xffff, .5))
 }
 
 animate();
